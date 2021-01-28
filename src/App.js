f6cbc1ab19b11433e1a6a90a79468546411901b4
Ola-A-Aldroubi <ola.droubi.91@gmail.com>
2021-01-28 15:01:24 +0000

    clearing the input field after submitting

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import './App.css';
-import React from 'react';
-import AddTask from './components/AddTask';
-import DeleteTask from './components/DeleteTask';
+import "./App.css";
+import React from "react";
+import AddTask from "./components/AddTask";
+import DeleteTask from "./components/DeleteTask";
 
 class App extends React.Component {
   state = {
-    tasks: []
-  }
+    tasks: [],
+  };
 
   addTask = (newTask) => {
-    this.setState((currentState) => {
-      return {tasks: [...currentState.tasks, newTask]}
-    }, () => {
-      console.log(this.state.tasks)
-    })
-  }
+    this.setState(
+      (currentState) => {
+        return { tasks: [...currentState.tasks, newTask] };
+      },
+      () => {
+        console.log(this.state.tasks);
+      }
+    );
+  };
 
   render() {
     return (
-    <main>
-    <h1>To-do List</h1>
+      <main>
+        <h1>To-do List</h1>
 
-    <AddTask add={this.addTask}/>
+        <AddTask add={this.addTask} />
 
-    <div>
-      <ul>Task List
-      {this.state.tasks.map((task) => {
-        return (
-          <li id={task}>{task}<DeleteTask toDelete={task}/></li>
-        )
-      })}
-      </ul>
-    </div>
-    </main>
-    )};
+        <div>
+          <ul>
+            Task List
+            {this.state.tasks.map((task) => {
+              return (
+                <li key={task}>
+                  {task}
+                  <DeleteTask toDelete={task} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
